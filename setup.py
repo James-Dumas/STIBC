26f3059616c7ddd427bd82a7f1ba4110da45c132
James Dumas <James-Dumas@users.noreply.github.com>
2017-02-16 17:54:06 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='Simple TI-BASIC Compiler',
+	version='1.0',
+	description="TI-BASIC Compiler/Decompiler",
+	author='James Dumas',
+	url='https://github.com/James-Dumas/STIBC',
+	license="GNU General Public License",
+	packages=['src',],
+	scripts=["src/stibc", "src/stibc-tokens.json"]
+)
